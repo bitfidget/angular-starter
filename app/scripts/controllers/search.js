54b0fa7945b702e12c89d179ab41f73c5535c5c1
=-kriss-= <kriss@modelcitizen.com.au>
2015-05-29 13:21:13 +1000

    added more methods

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -13,6 +13,8 @@
 
       // construct params object prior to making call
 
+      // add number for lazy load
+
       var params = {
         q : searchString,
         num : 50
@@ -20,12 +22,37 @@
 
       // RestService - send endpoint, params as object, error message
 
-      RestService.getData('search', params, 'searchFail').then(function(res) {
+      RestService.getData('search', params, 'searchGetFail').then(function(res) {
         console.log(res['search-results'])
-      })
+      });
+
+
+      // console.log($scope.searchResult)
+
+    }
+
+    $scope.searchCompletePost = function(searchString) {
+
+      // construct params object prior to making call
+
+      // add number for lazy load
+
+      var params = {
+        q : searchString,
+        num : 50
+      }
+
+      RestService.postData('search', params, 'searchPostFail').then(function(res) {
+        console.log(res['search-results'])
+      });
+
 
       // console.log($scope.searchResult)
 
     }
     
+    //next - autocomplete!
+    //http://www.justgoscha.com/allmighty-autocomplete/
+
+
   });
--- app/scripts/services/rest-service.js
+++ app/scripts/services/rest-service.js
@@ -1,20 +1,44 @@
 'use strict';
 
 app.factory('RestService', function(Restangular, $q) {
+    
     return {
       getData: function(endPoint, params, errMsg) {
+
+        var deferred = $q.defer();
 
         console.log('get data from ' + endPoint + params)
 
         // returns deferred promise once it arrives, handles errors separately - appends params nicely
         
-        var deferred = $q.defer();
+        
         Restangular.one(endPoint + '.json').get(params).then(function(res) {
           deferred.resolve(res);
         }, function(err) {
           console.log(errMsg, err.status, err.statusText)
         });
-        return deferred.promise;
+        return deferred.promise; 
+        
+      },
+      postData: function(endPoint, params, errMsg) {
+
+        var deferred = $q.defer();
+
+        console.log('post data to ' + endPoint + params)
+
+        // returns deferred promise once it arrives, handles errors separately - appends params nicely
+        
+      
+        Restangular.all(endPoint).post(params).then(function(res) {
+          deferred.resolve(res);
+        }, function(err) {
+          console.log(errMsg, err.status, err.statusText)
+        });
+        return deferred.promise; 
+      },
+      putData: function(endPoint, params, errMsg) {
+        Restangular.all('Partners').one('Password').customPUT(obj);
       }
+      
     };
   });
